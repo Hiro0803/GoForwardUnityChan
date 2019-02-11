1373507e807185095154afd857dfb900b5bd8ad2
山本裕公 <appleandapril2212@gmail.com>
2019-02-12 00:06:38 +0900

    GoForwardUnityChan - Block音の再生についての質問
    CubeにAudio Sourceをアタッチし、Cubeのタグとコリジョンした時にのみ音が再生するように記述したのですが、
    音の再生のタイミングがおかしいので、ご確認いただいてもよろしいでしょうか。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    private AudioSource sound01;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //AudioSourceコンポーネントを取得し、変数に格納
+        sound01 = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.tag == "Cube")
+        {
+            sound01.PlayOneShot(sound01.clip);
+        }
+    }
+
+}
